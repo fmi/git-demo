cb18ebece449ccfb4645976df8c5fac521ddc220
Stefan Kanev <stefan.kanev@gmail.com>
2012-04-26 20:47:01 +0300

    primes

---
+++ primes.py
@@ -0,0 +1,107 @@
+def primes():
+    1
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for baba in numbers:
+        print(baba)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = []
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+def primes():
+    numbers = [2, 3, 5, 7, 11, 13]
+    for number in numbers:
+        print(number)
+
+
+hehehehe
